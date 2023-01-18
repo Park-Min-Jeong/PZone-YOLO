baa4afb7073dd3f2538681dd3e9bed13b5aafdc6
ParkMinjeong <minwjd1028@g.skku.edu>
2023-01-18 20:09:26 +0900

    230118_2009 - remove yolo_temp.py - arrange yolo.py

--- FastModel/yolo/yolo.py
+++ FastModel/yolo/yolo.py
@@ -6,9 +6,8 @@ import numpy as np
 # Load Yolo
 
 def Detector(filename):
-    BASE_DIR = "C:/Users/smile/Documents/FastModel/yolo"
+    BASE_DIR = "D:/study/Git/YOLO-Test/FastModel/yolo"
     net = cv2.dnn.readNet(os.path.join(BASE_DIR,"backup/yolov4-tiny-custom_best.weights"),os.path.join(BASE_DIR,"backup/yolov4-tiny-custom.cfg"))
-    classes = []
     with open(os.path.join(BASE_DIR,"backup/ClassNames.names"), "r") as f:
         classes = [line.strip() for line in f.readlines()]
     layer_names = net.getLayerNames()
@@ -20,7 +19,6 @@ def Detector(filename):
     # img=Image.open(os.path.join(PHT_DIR,filename))
     # img.show()
     img = cv2.imread(filename)
-    # img = cv2.resize(img, None, fx=0.4, fy=0.4)
     height, width, channels = img.shape
 
     # Detecting objects
@@ -30,7 +28,6 @@ def Detector(filename):
 
     # Showing informations on the screen
     class_ids = []
-    confidences = []
     boxes = []
     for out in outs:
         for detection in out:
@@ -38,29 +35,11 @@ def Detector(filename):
             class_id = int(np.argmax(scores))
             confidence = scores[class_id]
             if confidence > 0.5:
-                # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-                # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
+                x = int(int(detection[0] * width) - w / 2)
+                y = int(int(detection[1] * height) - h / 2)
                 boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
     return {"length": len(class_ids), "Kinds":list(set(class_ids))}
-    # font = cv2.FONT_HERSHEY_PLAIN
-    # for i in range(len(boxes)):
-    #     if i in indexes:
-    #         x, y, w, h = boxes[i]
-    #         label = str(classes[class_ids[i]])
-    #         print(colors)
-    #         color = colors[i]
-    #         cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-    #         cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
-    # cv2.imshow("Image", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
