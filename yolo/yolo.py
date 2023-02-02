5b4e72086102c05ee5f81b64d0e3027aa61385e2
ParkMinjeong <minwjd1028@g.skku.edu>
2023-02-02 10:52:21 +0900

    230202 add surface model, arrange directory

---
+++ yolo/yolo.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import numpy as np
+
+# Load Yolo
+
+def Detector(filename):
+    BASE_DIR = "/home/YOLO-Test/yolo"
+    # BASE_DIR = "d:/study/Git/YOLO-Test/yolo"
+    net = cv2.dnn.readNetFromDarknet(os.path.join(BASE_DIR, "backup/surface.cfg"), os.path.join(BASE_DIR, "backup/surface_final.weights"))
+    # with open(os.path.join(BASE_DIR,"backup/surface.names"), "r") as f:
+    #     classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    # colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # Loading image
+    # PHT_DIR = "C:/Users/smile/Documents/FastModel/photo"
+    # img=Image.open(os.path.join(PHT_DIR,filename))
+    # img.show()
+    img = cv2.imread(filename)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = int(np.argmax(scores))
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(int(detection[0] * width) - w / 2)
+                y = int(int(detection[1] * height) - h / 2)
+                boxes.append([x, y, w, h])
+                class_ids.append(class_id)
+
+    return {"length": len(class_ids), "Kinds": list(set(class_ids))}
